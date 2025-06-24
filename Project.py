764d700737ee656530b3b47a9c7fe15e2a4065e7
JuliaF23 <julia.martins.figueiredo@hotmail.com>
2025-06-24 09:23:31 -0700

    Changing empty task

--- Project.py
+++ Project.py
@@ -4,16 +4,11 @@ tasks = []
 # Function to add a task
 def add_task():
     task = input("Enter the task: ")  # Ask the user for the task
-    tasks.append(task)  # Add task to the list
-    print(f"'{task}' has been added to the list.")
-
-def view_tasks():
-    print("Your Tasks")
-    if not tasks:
-        print("There are no tasks yet")
+    if task.strip() == "":  # Check if task is empty or only spaces
+        print("The task is empty. Please try again.")
     else:
-        for i, task in enumerate(tasks,1):
-            print(f"{i}. {task}")
+        tasks.append(task)  # Add task to the list
+        print(f"'{task}' has been added to the list.")  
 
 # Loop for the menu
 while True:
@@ -22,21 +17,18 @@ while True:
     print("2. Remove Task")
     print("3. View Tasks")
     print("4. Exit")
-
+    
     # Ask the user for their choice
     choice = input("Enter your choice: ")
-
+    
     if choice == "1":
         add_task()
     elif choice == "2":
-        print("You chose to remove a task.")
+        print("You chose to remove a task.")  
     elif choice == "3":
-        print("You chose to view tasks.")
-        view_tasks()
+        print("You chose to view tasks.")  
     elif choice == "4":
         print("Exiting the application.")
         break
     else:
         print("Invalid choice. Please try again.")
-
-
