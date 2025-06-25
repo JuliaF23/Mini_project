db615a3a97ae0a353074c98475f2e7f41d0e5574
Yuki Okabe <s.n.w20240703@gmail.com>
2025-06-24 19:27:41 -0700

    Added a function for suggested tasks

--- Project.py
+++ Project.py
@@ -1,3 +1,5 @@
+import datetime as dt
+
 # Empty list to store tasks
 tasks = []
 
@@ -23,6 +25,31 @@ def add_task():
     # Confirm to the user
     print(f"'{task}' with priority '{priority}' and deadline '{deadline}' has been added to the list.")
 
+# Function to suggest tasks
+def suggest_task():
+    suggest_tasks = []
+    for task in tasks:
+        # Skip when the deadline is not the expected data type
+        try:
+            date_deadline = dt.datetime.strptime(task["deadline"], "%Y-%m-%d").date()
+        except:
+            continue
+
+        # Add to another list when the priority is high or it is within 7 days of the deadline
+        today = dt.date.today()
+        if date_deadline >= today and (task["priority"] == "high" or date_deadline <= today + dt.timedelta(days=7)):
+            suggest_tasks.append(task)
+    
+    if not suggest_tasks:
+        print("No suggestions for now. Enjoy the calm!")
+    else:
+        print("Here are some tasks you might want to work!")
+    
+    # Print the message by closest deadline
+    suggest_tasks.sort(key=lambda task: task["deadline"])
+    for task in suggest_tasks:
+        print(f"{task["name"]} - {task["priority"]} - {task["deadline"]}")
+
 # Create a loop for the menu
 while True:
     print("\nAdvanced To-Do List Application")
@@ -42,7 +69,8 @@ while True:
     elif choice == "3":
         print("You chose to view tasks.")  
     elif choice == "4":
-        print("You chose to suggest tasks.")  
+        print("You chose to suggest tasks.")
+        suggest_task()
     elif choice == "5":
         print("Exiting the application.")
         break
